340d32a915f871789f0e764e73482dff1c84c65c
lalit <lalitvijay9480@gmail.com>
2024-12-18 14:49:40 +0530

    finish aoc day 2

--- aoc-24/02/main.c
+++ aoc-24/02/main.c
@@ -3,7 +3,18 @@
 #include <string.h>
 #include <time.h>
 
-int is_report_safe(int *report, int len) {
+int *remove_element(int *arr, int index, int len) {
+  int *new_arr = (int *)malloc((len - 1) * sizeof(int));
+  for (int i = 0, j = 0; i < len; i++) {
+    if (i != index) {
+      new_arr[j++] = arr[i];
+    }
+  }
+  return new_arr;
+}
+
+int is_report_safe(int *report, int len, int initial_call) {
+
   int is_safe = 1;
   int inc = report[1] - report[0] > 0 ? 1 : 0;
   for (int i = 0; i < len; i++) {
@@ -20,6 +31,21 @@ int is_report_safe(int *report, int len) {
       }
     }
   }
+
+  if (!is_safe && initial_call) {
+    for (int k = 0; k < len; k++) {
+      int *new_arr = remove_element(report, k, len);
+      if (is_report_safe(new_arr, len - 1, 0)) {
+        /*for (int o = 0; o < len - 1; o++) {*/
+        /*  printf("%d ", new_arr[o]);*/
+        /*}*/
+        free(new_arr);
+        return 1;
+      }
+      free(new_arr);
+    }
+  }
+
   return is_safe;
 }
 
@@ -45,7 +71,7 @@ int main() {
       report[i++] = atoi(token);
       token = strtok(NULL, " ");
     }
-    if (is_report_safe(report, i)) {
+    if (is_report_safe(report, i, 1)) {
       safe_reports_count += 1;
     }
   }
